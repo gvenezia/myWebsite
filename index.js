04766d5a437930b172104e87063a07497405b258
Gaetano <g.venezia@outlook.com>
2018-01-10 11:40:01 -0600

    Cleaned up the index.html, added index.js to work on scrolling animation, commented out for now

---
+++ index.js
@@ -0,0 +1,22 @@
+// window.addEventListener("load", function() {
+//   var about = document.querySelector("#about");
+//   var git = document.querySelector("#git");
+//   var contact = document.querySelector("#contact");
+
+//   var aboutLink = document.querySelector(".abt-link");
+//   var gitLink = document.querySelector(".git-link");
+//   var contactLink = document.querySelector(".contact-link");
+
+//   document.querySelector(".abt-link").addEventListener("click", function() {
+//     about.scrollIntoView({top: 1000, left: 0, behavior:'smooth'});
+//   });
+
+//   gitLink.addEventListener("click", function() {
+//     git.scrollIntoView({top: 1000, left: 0, behavior:'smooth'});
+//   });
+
+//   contactLink.addEventListener("click", function() {
+//     contact.scrollIntoView({behavior:'smooth'});
+//   });
+
+// });
